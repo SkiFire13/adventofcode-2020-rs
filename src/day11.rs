e96679691e4f981397bfedc8c125179c1964e98a
Giacomo Stevanato <giaco.stevanato@gmail.com>
2020-12-11 10:37:20 +0100

    Greatly improved day11 solution performance

--- src/day11.rs
+++ src/day11.rs
@@ -6,66 +6,70 @@ type Input = Grid<CellPos>;
 pub enum CellPos {
     EmptySeat,
     OccupiedSeat,
-    Floor
+    Floor,
 }
 
 pub fn input_generator(input: &str) -> Input {
-    
-    Grid {
-        vec: input.lines()
-            .flat_map(|line| line.chars())
-            .map(|c| match c {
-                '.' => CellPos::Floor,
-                'L' => CellPos::EmptySeat,
-                '#' => CellPos::OccupiedSeat,
-                _ => panic!(),
-            })
-            .collect(),
-        width: input.lines().next().unwrap().len()
+    let width = input.lines().next().unwrap().len() + 2;
+    let mut vec = Vec::with_capacity(input.len() + 3 * width);
+
+    vec.extend(iter::repeat(CellPos::Floor).take(width));
+    for line in input.lines() {
+        vec.push(CellPos::Floor);
+        vec.extend(line.chars().map(|c| match c {
+            '.' => CellPos::Floor,
+            'L' => CellPos::EmptySeat,
+            '#' => CellPos::OccupiedSeat,
+            _ => panic!(),
+        }));
+        vec.push(CellPos::Floor);
     }
+    vec.extend(iter::repeat(CellPos::Floor).take(width));
+    
+    Grid { vec, width }
 }
 
 fn evolve(
-    input: &Grid<CellPos>, 
-    max_occupied_seats: usize,
+    input: &Grid<CellPos>,
+    max_occupied: usize,
     mut sees_occupied: impl FnMut(&Grid<CellPos>, isize, isize, isize, isize) -> bool,
 ) -> usize {
     let mut grid = input.clone();
     let mut new_grid = grid.clone();
-    
+
     let mut changed = true;
     while changed {
         changed = false;
 
-        for x in 0..grid.width {
-            for y in 0..grid.height() {
-                let nearly_occupied = {
-                    let x = x as isize;
-                    let y = y as isize;
-                    (-1..=1)
-                        .flat_map(|dx| (-1..=1).map(move |dy| (dx, dy)))
-                        .filter(|&(dx, dy)| !(dx == 0 && dy == 0))
-                        .filter(|(dx, dy)| !(dx + x < 0 || dy + y < 0) )
-                        .filter(|&(dx, dy)| sees_occupied(&grid, x, y, dx, dy))
-                        .count()
-                };
+        for y in 1..grid.height() - 1 {
+            for x in 1..grid.width - 1 {
+                if grid[(x, y)] != CellPos::Floor {
+                    let n_occupied = [
+                        (-1, -1),
+                        (0, -1),
+                        (1, -1),
+                        (-1, 0),
+                        (1, 0),
+                        (-1, 1),
+                        (0, 1),
+                        (1, 1),
+                    ]
+                    .iter()
+                    .copied()
+                    .filter(|&(dx, dy)| sees_occupied(&grid, x as isize, y as isize, dx, dy))
+                    .count();
 
-                match grid[(x, y)] {
-                    CellPos::EmptySeat => {
-                        new_grid[(x, y)] = CellPos::EmptySeat;
-                        if nearly_occupied == 0 {
-                            new_grid[(x, y)] = CellPos::OccupiedSeat;
+                    new_grid[(x, y)] = match grid[(x, y)] {
+                        CellPos::EmptySeat if n_occupied == 0 => {
                             changed = true;
+                            CellPos::OccupiedSeat
                         }
-                    }
-                    CellPos::OccupiedSeat => {
-                        new_grid[(x, y)] = CellPos::OccupiedSeat;
-                        if nearly_occupied >= max_occupied_seats {
-                            new_grid[(x, y)] = CellPos::EmptySeat;
+                        CellPos::OccupiedSeat if n_occupied >= max_occupied => {
                             changed = true;
+                            CellPos::EmptySeat
                         }
+                        cellpos => cellpos,
                     }
-                    _ => {}
                 }
             }
         }
@@ -73,31 +77,26 @@ fn evolve(
         mem::swap(&mut grid, &mut new_grid);
     }
 
-    grid.vec.iter().filter(|&&cellpos| cellpos == CellPos::OccupiedSeat).count()
+    grid.vec
+        .iter()
+        .filter(|&&cellpos| cellpos == CellPos::OccupiedSeat)
+        .count()
 }
 
 pub fn part1(input: &Input) -> usize {
     evolve(input, 4, |grid, x, y, dx, dy| {
-        grid.get(((x+dx)as usize, (y+dy) as usize)) == Some(&CellPos::OccupiedSeat)
+        grid.get(((x + dx) as usize, (y + dy) as usize)) == Some(&CellPos::OccupiedSeat)
     })
 }
 
 pub fn part2(input: &Input) -> usize {
     evolve(input, 5, |grid, x, y, dx, dy| {
-        let mut x = x+dx;
-        let mut y = y+dy;
-        let mut found = false;
-        while x >= 0 && y >= 0 && !found {
-            match grid.get((x as usize, y as usize)) {
-                Some(CellPos::OccupiedSeat) => found = true,
-                Some(CellPos::EmptySeat) => break,
-                None => break,
-                _ => {
-                    x += dx;
-                    y += dy;
-                }
-            }
+        let mut x = x + dx;
+        let mut y = y + dy;
+        while let Some(CellPos::Floor) = grid.iget((x, y)) {
+            x += dx;
+            y += dy;
         }
-        found
+        grid.iget((x, y)) == Some(&CellPos::OccupiedSeat)
     })
 }
