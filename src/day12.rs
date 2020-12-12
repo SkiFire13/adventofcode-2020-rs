66df94c19c430e1dd1168b301edb9ed6f1f9ef9b
Giacomo Stevanato <giaco.stevanato@gmail.com>
2020-12-12 10:20:53 +0100

    Simplified day12 even more (+ little performance boost)

--- src/day12.rs
+++ src/day12.rs
@@ -4,27 +4,30 @@ type Input = Vec<Instruction>;
 
 #[derive(Copy, Clone)]
 pub enum Instruction {
-    North(i32),
-    South(i32),
-    East(i32),
-    West(i32),
+    Move((i32, i32)),
     Forward(i32),
-    Left(i32),
-    Right(i32),
+    Rotate(i32),
 }
 
 pub fn input_generator(input: &str) -> Input {
     input
         .lines()
-        .map(|line| match line.chars().next() {
-            Some('N') => Instruction::North(line[1..].parse().expect("Invalid input")),
-            Some('S') => Instruction::South(line[1..].parse().expect("Invalid input")),
-            Some('E') => Instruction::East(line[1..].parse().expect("Invalid input")),
-            Some('W') => Instruction::West(line[1..].parse().expect("Invalid input")),
-            Some('F') => Instruction::Forward(line[1..].parse().expect("Invalid input")),
-            Some('L') => Instruction::Left(line[1..].parse().expect("Invalid input")),
-            Some('R') => Instruction::Right(line[1..].parse().expect("Invalid input")),
-            _ => panic!("Invalid input")
+        .map(|line| {
+            let c = line.chars().next().expect("Invalid input");
+            let n = line
+                .get(1..)
+                .and_then(|n| n.parse().ok())
+                .expect("Invalid input");
+            match c {
+                'N' => Instruction::Move((0, n)),
+                'S' => Instruction::Move((0, -n)),
+                'E' => Instruction::Move((n, 0)),
+                'W' => Instruction::Move((-n, 0)),
+                'F' => Instruction::Forward(n),
+                'L' => Instruction::Rotate(n / 90 % 4),
+                'R' => Instruction::Rotate(4 - n / 90 % 4),
+                _ => panic!("Invalid input"),
+            }
         })
         .collect()
 }
@@ -35,24 +38,19 @@ pub fn part1(input: &Input) -> i32 {
 
     for &instr in input {
         match instr {
-            Instruction::North(n) => pos.1 += n,
-            Instruction::South(n) => pos.1 -= n,
-            Instruction::East(n) => pos.0 += n,
-            Instruction::West(n) => pos.0 -= n,
+            Instruction::Move((x, y)) => {
+                pos.0 += x;
+                pos.1 += y;
+            }
             Instruction::Forward(n) => {
                 pos.0 += n * dir.0;
                 pos.1 += n * dir.1;
             }
-            Instruction::Left(n) => {
-                for _ in 0..n/90 {
+            Instruction::Rotate(n) => {
+                for _ in 0..n {
                     dir = (-dir.1, dir.0);
                 }
-            },
-            Instruction::Right(n) => {
-                for _ in 0..n/90 {
-                    dir = (dir.1, -dir.0);
-                }
-            },
+            }
         }
     }
 
@@ -65,24 +63,19 @@ pub fn part2(input: &Input) -> i32 {
 
     for &instr in input {
         match instr {
-            Instruction::North(n) => way.1 += n,
-            Instruction::South(n) => way.1 -= n,
-            Instruction::East(n) => way.0 += n,
-            Instruction::West(n) => way.0 -= n,
+            Instruction::Move((x, y)) => {
+                way.0 += x;
+                way.1 += y;
+            }
             Instruction::Forward(n) => {
                 pos.0 += n * way.0;
                 pos.1 += n * way.1;
             }
-            Instruction::Left(n) => {
-                for _ in 0..n/90 {
+            Instruction::Rotate(n) => {
+                for _ in 0..n {
                     way = (-way.1, way.0);
                 }
-            },
-            Instruction::Right(n) => {
-                for _ in 0..n/90 {
-                    way = (way.1, -way.0);
-                }
-            },
+            }
         }
     }
 
