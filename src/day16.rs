d31ad10b88e3e3129901cb62bc28f9e3b95b52a3
Giacomo Stevanato <giaco.stevanato@gmail.com>
2020-12-16 15:31:57 +0100

    Simplify day16 solution Since it's not possible to have an unambiguous solution with no rows/columns that only have 1 candidate we can search only one of them

--- src/day16.rs
+++ src/day16.rs
@@ -76,7 +76,6 @@ pub fn part2(input: &Input) -> u64 {
     });
 
     let mut candidates = vec![true; rules.len() * rules.len()];
-    let mut v_lens = vec![rules.len(); rules.len()];
     let mut r_lens = vec![rules.len(); rules.len()];
 
     for nearbyticket in nearbytickets.iter() {
@@ -84,7 +83,6 @@ pub fn part2(input: &Input) -> u64 {
             for (rpos, &(_, r1s, r1e, r2s, r2e)) in rules.iter().enumerate() {
                 if !((r1s <= v && v <= r1e) || (r2s <= v && v <= r2e)) {
                     if mem::replace(&mut candidates[vpos + rules.len() * rpos], false) {
-                        v_lens[vpos] -= 1;
                         r_lens[rpos] -= 1;
                     }
                 }
@@ -94,36 +92,11 @@ pub fn part2(input: &Input) -> u64 {
 
     let mut associations = vec![usize::MAX; rules.len()];
     let mut associations_len = 0;
-    let mut found = true;
-    while found && associations_len < rules.len() {
-        found = false;
-        for v in 0..rules.len() {
-            if v_lens[v] == 1 {
-                found = true;
-                let r = candidates
-                    .iter()
-                    .skip(v)
-                    .step_by(rules.len())
-                    .position(|&b| b)
-                    .unwrap();
-                let prev = mem::replace(&mut associations[r], v);
-                assert_eq!(prev, usize::MAX, "Can't solve");
-                associations_len += 1;
-                for v in 0..rules.len() {
-                    if mem::replace(&mut candidates[v + r * rules.len()], false) {
-                        v_lens[v] -= 1;
-                        r_lens[r] -= 1;
-                    }
-                }
-            }
-        }
+    while associations_len < rules.len() {
         for r in 0..rules.len() {
             if r_lens[r] == 1 {
-                found = true;
-                let v = candidates
+                let v = candidates[r * rules.len()..(r + 1) * rules.len()]
                     .iter()
-                    .skip(r * rules.len())
-                    .take(rules.len())
                     .position(|&b| b)
                     .unwrap();
                 let prev = mem::replace(&mut associations[r], v);
@@ -131,7 +104,6 @@ pub fn part2(input: &Input) -> u64 {
                 associations_len += 1;
                 for r in 0..rules.len() {
                     if mem::replace(&mut candidates[v + r * rules.len()], false) {
-                        v_lens[v] -= 1;
                         r_lens[r] -= 1;
                     }
                 }
@@ -144,11 +116,6 @@ pub fn part2(input: &Input) -> u64 {
         .enumerate()
         .map(|(pos, (name, _, _, _, _))| (pos, name))
         .filter(|(_, name)| name.starts_with("departure"))
-        .map(|(pos, _)| {
-            myticket
-                .get(associations[pos])
-                .copied()
-                .expect("Can't solve")
-        })
+        .map(|(pos, _)| myticket[associations[pos]])
         .product()
 }
