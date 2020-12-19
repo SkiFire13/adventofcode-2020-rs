2de2df86f4aeff859003b7dbfdaac35b9b39701d
Giacomo Stevanato <giaco.stevanato@gmail.com>
2020-12-19 12:15:29 +0100

    Swap HashMap with Vec for increased performance in day19

--- src/day19.rs
+++ src/day19.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input<'a> = (HashMap<u8, Rule<'a>>, Vec<&'a str>);
+type Input<'a> = (Vec<Rule<'a>>, Vec<&'a str>);
 
 #[derive(Clone, Debug)]
 pub enum Rule<'a> {
@@ -16,27 +16,26 @@ pub fn input_generator(input: &str) -> Input {
             .collect()
     }
 
-    let (rules, tests) = input.split("\n\n").collect_tuple().expect("Invalid input");
-    let rules = rules
-        .lines()
-        .map(|line| {
-            let (n, rule) = line.split(": ").collect_tuple().expect("Invalid input");
-            let n = n.parse().expect("Invalid input");
-            let rule = if rule.starts_with('"') {
-                Rule::Literal(
-                    rule.strip_prefix('"')
-                        .unwrap()
-                        .strip_suffix('"')
-                        .expect("Invalid input"),
-                )
-            } else if let Some((rs1, rs2)) = rule.split(" | ").collect_tuple() {
-                Rule::Or(parse_rule_seq(rs1), parse_rule_seq(rs2))
-            } else {
-                Rule::Seq(parse_rule_seq(rule))
-            };
-            (n, rule)
-        })
-        .collect();
+    let (raw_rules, tests) = input.split("\n\n").collect_tuple().expect("Invalid input");
+    let mut rules = vec![Rule::Literal(""); 256];
+    for line in raw_rules.lines() {
+        let (n, rule) = line.split(": ").collect_tuple().expect("Invalid input");
+        let n = n.parse::<usize>().expect("Invalid input");
+        let rule = if rule.starts_with('"') {
+            Rule::Literal(
+                rule.strip_prefix('"')
+                    .unwrap()
+                    .strip_suffix('"')
+                    .expect("Invalid input"),
+            )
+        } else if let Some((rs1, rs2)) = rule.split(" | ").collect_tuple() {
+            Rule::Or(parse_rule_seq(rs1), parse_rule_seq(rs2))
+        } else {
+            Rule::Seq(parse_rule_seq(rule))
+        };
+        rules[n] = rule;
+    }
+
     let tests = tests.lines().collect();
     (rules, tests)
 }
@@ -74,9 +73,9 @@ impl<'a, T> ChunkStack<'a, T> {
 fn match_rule<'i>(
     s: &'i str,
     mut next: ChunkStack<u8>,
-    rules: &HashMap<u8, Rule>,
+    rules: &[Rule],
 ) -> Option<&'i str> {
-    match next.pop().map(|r| &rules[r]) {
+    match next.pop().map(|&r| &rules[r as usize]) {
         Some(Rule::Literal(lit)) => s.strip_prefix(lit).and_then(|s| match_rule(s, next, rules)),
         Some(Rule::Seq(seq)) => match_rule(s, next.append(seq), rules),
         Some(Rule::Or(seq1, seq2)) => match_rule(s, next.append(seq1), rules)
@@ -98,19 +97,13 @@ pub fn part2(input: &Input) -> usize {
     let (rules, tests) = input;
 
     let mut rules = rules.clone();
-    rules.insert(
-        8,
-        Rule::Or(
-            [42].iter().copied().collect(),
-            [42, 8].iter().copied().collect(),
-        ),
+    rules[8] = Rule::Or(
+        [42].iter().copied().collect(),
+        [42, 8].iter().copied().collect(),
     );
-    rules.insert(
-        11,
-        Rule::Or(
-            [42, 31].iter().copied().collect(),
-            [42, 11, 31].iter().copied().collect(),
-        ),
+    rules[11] = Rule::Or(
+        [42, 31].iter().copied().collect(),
+        [42, 11, 31].iter().copied().collect(),
     );
 
     tests
