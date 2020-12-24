2274ed3d3e8e451e22558d1640a0da981c42c7b0
Giacomo Stevanato <giaco.stevanato@gmail.com>
2020-12-24 10:16:31 +0100

    Cleaned and improved day24 solution

--- src/day24.rs
+++ src/day24.rs
@@ -9,35 +9,38 @@ pub enum Dir {
     NW,
     NE,
     SW,
-    SE
+    SE,
 }
 
 pub fn input_generator(input: &str) -> Input {
-    input.lines().map(|line| {
-        let mut path = Vec::new();
-        let mut chars = line.chars();
-        while let Some(c) = chars.next() {
-            match c {
-                'e' => path.push(Dir::E),
-                'w' => path.push(Dir::W),
-                's' => path.push(match chars.next() {
-                    Some('e') => Dir::SE,
-                    Some('w') => Dir::SW,
-                    _ => panic!(),
-                }),
-                'n' => path.push(match chars.next() {
-                    Some('e') => Dir::NE,
-                    Some('w') => Dir::NW,
-                    _ => panic!(),
-                }),
-                _ => panic!(),
+    input
+        .lines()
+        .map(|line| {
+            let mut path = Vec::new();
+            let mut chars = line.chars();
+            while let Some(c) = chars.next() {
+                match c {
+                    'e' => path.push(Dir::E),
+                    'w' => path.push(Dir::W),
+                    's' => path.push(match chars.next() {
+                        Some('e') => Dir::SE,
+                        Some('w') => Dir::SW,
+                        _ => panic!("Invalid input"),
+                    }),
+                    'n' => path.push(match chars.next() {
+                        Some('e') => Dir::NE,
+                        Some('w') => Dir::NW,
+                        _ => panic!("Invalid input"),
+                    }),
+                    _ => panic!("Invalid input"),
+                }
             }
-        }
-        path
-    }).collect()
+            path
+        })
+        .collect()
 }
 
-pub fn part1(input: &Input) -> usize {
+fn make_tiles(input: &[Vec<Dir>]) -> FxHashMap<(i32, i32), bool> {
     let mut tiles = FxHashMap::default();
     for path in input {
         let mut pos = (0, 0);
@@ -46,70 +49,39 @@ pub fn part1(input: &Input) -> usize {
                 Dir::E => pos.0 += 1,
                 Dir::W => pos.0 -= 1,
                 Dir::SE => pos.1 -= 1,
-                Dir::SW => {
-                    pos.1 -= 1;
-                    pos.0 -= 1;
-                }
+                Dir::SW => pos = (pos.0 - 1, pos.1 - 1),
                 Dir::NW => pos.1 += 1,
-                Dir::NE => {
-                    pos.0 += 1;
-                    pos.1 += 1;
-                }
+                Dir::NE => pos = (pos.0 + 1, pos.1 + 1),
             }
         }
-
-        let entry = tiles.entry(pos).or_insert(false);
-        *entry = !*entry; 
+        *tiles.entry(pos).or_insert(false) ^= true;
     }
+    tiles
+}
 
+pub fn part1(input: &Input) -> usize {
+    let tiles = make_tiles(input);
     tiles.iter().filter(|&(_, &v)| v).count()
 }
 
 pub fn part2(input: &Input) -> usize {
-    let mut tiles = FxHashMap::default();
-    for path in input {
-        let mut pos = (0, 0);
-        for &dir in path {
-            match dir {
-                Dir::E => pos.0 += 1,
-                Dir::W => pos.0 -= 1,
-                Dir::SE => pos.1 -= 1,
-                Dir::SW => {
-                    pos.1 -= 1;
-                    pos.0 -= 1;
-                }
-                Dir::NW => pos.1 += 1,
-                Dir::NE => {
-                    pos.0 += 1;
-                    pos.1 += 1;
-                }
-            }
-        }
-
-        let entry = tiles.entry(pos).or_insert(false);
-        *entry = !*entry; 
-    }
-
+    let mut tiles = make_tiles(input);
     let mut adj = FxHashMap::default();
-
     for _ in 0..100 {
-        adj.clear();
+        adj.values_mut().for_each(|count| *count = 0);
         for (&pos, &black) in tiles.iter() {
             if black {
                 adj.entry(pos).or_insert(0);
-                *adj.entry((pos.0 + 1, pos.1)).or_insert(0) += 1;
-                *adj.entry((pos.0 - 1, pos.1)).or_insert(0) += 1;
-                *adj.entry((pos.0, pos.1 + 1)).or_insert(0) += 1;
-                *adj.entry((pos.0, pos.1 - 1)).or_insert(0) += 1;
-                *adj.entry((pos.0 + 1, pos.1 + 1)).or_insert(0) += 1;
-                *adj.entry((pos.0 - 1, pos.1 - 1)).or_insert(0) += 1;
+                for &(dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1)].iter() {
+                    *adj.entry((pos.0 + dx, pos.1 + dy)).or_insert(0) += 1;
+                }
             }
         }
 
         for (&pos, &n_black) in adj.iter() {
             let was_black = tiles.get(&pos).copied().unwrap_or(false);
             if was_black && (n_black == 0 || n_black > 2) {
-                tiles.entry(pos).and_modify(|is_black| *is_black = false);
+                tiles.insert(pos, false);
             } else if !was_black && n_black == 2 {
                 tiles.insert(pos, true);
             }
